36c0133fc07fba7fb7b8d7e014187db9e4c84410
lee <lishengmin857@gmail.com>
2026-04-30 15:28:46 +0800

    feat: add Docker configuration and deployment automation scripts for backend containerization

---
+++ scripts/archive.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -cvf admin-backend.tar --exclude=node_modules --exclude=.next --exclude=.git .
---
+++ scripts/scp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp admin-backend.tar root@49.235.172.63:/admin-backend/
